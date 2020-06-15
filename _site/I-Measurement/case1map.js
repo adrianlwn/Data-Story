27bc970b1448a46c16202b4982e4c7e1c68ce68b
Adrian Löwenstein <adrian.loewenstein@icloud.com>
2020-06-15 23:43:18 +0200

    hardcoded

--- _site/I-Measurement/case1map.js
+++ _site/I-Measurement/case1map.js
@@ -217,8 +217,8 @@ function updateMap1(variableSelected,normalized) {
   var form = d3.format(",")
 
 
-  var path_normalized_json = "./I-Measurement/measurement_norm.json";
-  var path_json = "./I-Measurement/measurement.json";
+  var path_normalized_json = "/Tweet-Awareness-Data-Story/I-Measurement/measurement_norm.json";
+  var path_json = "/Tweet-Awareness-Data-Story/I-Measurement/measurement.json";
   if (normalized == 'tweet_normalized'){
     var path_output_json = path_normalized_json;
   }
